1072347730840c37ea3559a3d399f696aa59efed
Ben Franske <ben.mm@franske.com>
2024-12-18 10:43:00 -0600

    fix playbook for 2480 check bash script

--- static/2480check.sh
+++ static/2480check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#This script should be executed like: sudo wget -qO- https://info.ihitc.net/2480check.sh | bash
+
 # Check if the script is running as root
 if [ "$EUID" -ne 0 ]; then
   echo "This command must be run as root or with sudo. Exiting, please run again with appropriate permissions."
